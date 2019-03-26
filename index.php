0092449708c199c2a8a8e1088b6407133ae2bf4b
Tran Tan Phat <p.ketoul@gmail.com>
2019-03-26 14:03:26 +0700

    Them css

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="cite.css"/>
     <title>CHÀO MỪNG</title>
 </head>
 <body>
-    Chào mừng đến với trang web của tôi!!!
+    <h2>Chào mừng đến với trang web của tôi!!!</h2>
+
 </body>
 </html>
