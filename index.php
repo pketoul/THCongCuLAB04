21a93255e1b59867ccd5be9cf0c6b510e23e2f86
“quyitbmt” <hoangquyitbmt@gmail.com>
2019-03-26 14:22:08 +0700

    New Para Update

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 </head>
 <body>
     <h2>Chào mừng đến với trang web của tôi!!!</h2>
+    <p>Shapely is a powerful and versatile one page WordPress theme with pixel perfect design and outstanding functionality. It is by far the most advanced free WordPress theme available today with loads of unmatched customization options. This theme comes with several homepage widgets that can be used to add portfolio, testimonials, parallax sections, your product or service information, call for action and much more.
 
+Shapely supports most free and premium WordPress plugins such as WooCommerce, Jetpack, Gravity Forms, Contact Form 7, Yoast SEO, Google Analytics by Yoast and much more. Needless to say that theme is mobile friendly and responsive that makes it look stunning on any device.
+
+This theme is the best suited for business, landing page, portfolio, ecommerce, store, local business, personal websites but can be tweaked to be used as blog, magazine or any other awesome website while highlighting its unique one page setup. This is going to be the last WordPress theme you will ever want to use because it is so much better than anything you have seen and it will deliver the best SEO results since we have well optimized it structure.</p>
 </body>
 </html>
